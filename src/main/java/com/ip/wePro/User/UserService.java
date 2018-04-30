3c62af69a9be7bfdf7a538c325ba2075d24f33b6
sushant <sushantjhingan@hotmail.com>
2018-04-30 05:25:34 -0400

    added custom skills removed

--- src/main/java/com/ip/wePro/User/UserService.java
+++ src/main/java/com/ip/wePro/User/UserService.java
@@ -1,13 +1,25 @@
 package com.ip.wePro.User;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.ip.wePro.skills.Skills;
+import com.ip.wePro.userSkills.UserSkills;
+import com.ip.wePro.userSkills.UserSkillsRepository;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+    
+    @Autowired
+    private UserSkillsRepository userSkillsRepository;
 
     public User getUserProfile(int id) {
         return userRepository.findById(id).get();
@@ -39,5 +51,27 @@ public class UserService {
         userRepository.save(user);
         return 1;
     }
+
+
+    @Transactional
+	public int updateUserSkills(List<Skills> skills, Integer uid) {
+		// TODO Auto-generated method stub
+    	
+    	List<UserSkills> userSkills = userSkillsRepository.findAllByUserId(uid);
+    	
+    	for(UserSkills us: userSkills){
+    		us.setRemoved(true);
+    		userSkillsRepository.save(us);
+    	}
+    	for(Skills us: skills){
+    		UserSkills userSk = new UserSkills();
+    		userSk.setRemoved(false);
+    		userSk.setUser_id(uid);
+    		userSk.setSkill(us);
+    		userSkillsRepository.save(userSk);
+    	}
+		
+		return 1;
+	}
 
 }
